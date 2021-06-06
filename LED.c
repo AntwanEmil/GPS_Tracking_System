2f19854981d6a2b25ee9a032e5c8b0c87eacec16
unknown <1701037@eng.asu.edu.eg>
2021-06-06 20:59:31 +0200

    led comments

--- LED.c
+++ LED.c
@@ -2,20 +2,20 @@
 #include "stdint.h"
 
 void portF_Init(void){
-	SYSCTL_RCGCGPIO_R |= 0x20;
-	while ((SYSCTL_RCGCGPIO_R & 0x20) == 0){ }
-	GPIO_PORTF_LOCK_R = 0x4C4F434B;   
-	GPIO_PORTF_AFSEL_R = 0;            
-	GPIO_PORTF_PCTL_R = 0;           
-	GPIO_PORTF_AMSEL_R = 0;            
-	GPIO_PORTF_CR_R = 0x1F;
-	GPIO_PORTF_DIR_R = 0X0E;           
-	GPIO_PORTF_PUR_R = 0x11;
-	GPIO_PORTF_DEN_R = 0X1F;	
+	SYSCTL_RCGCGPIO_R |= 0x20;   //enable clock for Port_F
+	while ((SYSCTL_RCGCGPIO_R & 0x20) == 0){ }  //allow time for Port_F activation
+	GPIO_PORTF_LOCK_R = 0x4C4F434B;  //unlock
+	GPIO_PORTF_AFSEL_R = 0;          //used as GPIO
+	GPIO_PORTF_PCTL_R = 0;           //not used
+	GPIO_PORTF_AMSEL_R = 0;          //not analog
+	GPIO_PORTF_CR_R = 0x1F;          //allow changes to all pins
+	GPIO_PORTF_DIR_R = 0X0E;         //set pins 1, 2, 3 outputs
+	GPIO_PORTF_PUR_R = 0x11;         //enable pull up resistors for switches at pins 0 and 4
+	GPIO_PORTF_DEN_R = 0X1F;	       //all pins are digital
 	
 }
 
 void LED_ON(uint32_t distance){
-	if(distance>100)
-	GPIO_PORTF_DATA_R = 0x02;
+	if(distance > 100)
+	GPIO_PORTF_DATA_R = 0x02;   //turn red led on if distance exceeds 100 meters
 }
--- uart.d
+++ uart.d
@@ -1,3 +1,3 @@
 .\uart.o: UART.c
 .\uart.o: tm4c123gh6pm.h
-.\uart.o: F:\ARM\ARMCC\bin\..\include\stdint.h
+.\uart.o: E:\Courses_Talta 7asebaat\2nd Term\Micro Processors\Keil 4\ARM\ARMCC\bin\..\include\stdint.h
