20233af2c132f788981a569bb1bbb8ea026e0a24
AntoineZakaria <tonyzakaria53@gmail.com>
2021-06-11 12:34:00 +0200

    edit

--- systick.d
+++ systick.d
@@ -1,3 +1,3 @@
 .\systick.o: SYSTICK.c
 .\systick.o: tm4c123gh6pm.h
-.\systick.o: E:\Courses_Talta 7asebaat\2nd Term\Micro Processors\Keil 4\ARM\ARMCC\bin\..\include\stdint.h
+.\systick.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
