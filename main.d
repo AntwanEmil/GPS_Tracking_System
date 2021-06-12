f8381864dbd89ff40465b5823accb827e5a7cd28
unknown <1701037@eng.asu.edu.eg>
2021-06-12 22:18:24 +0200

    edit main

--- main.d
+++ main.d
@@ -4,7 +4,9 @@
 .\main.o: UART.h
 .\main.o: LCD.h
 .\main.o: E:\Courses_Talta 7asebaat\2nd Term\Micro Processors\Keil 4\ARM\ARMCC\bin\..\include\string.h
+.\main.o: LED.h
 .\main.o: SYSTICK.h
 .\main.o: E:\Courses_Talta 7asebaat\2nd Term\Micro Processors\Keil 4\ARM\ARMCC\bin\..\include\stdio.h
 .\main.o: E:\Courses_Talta 7asebaat\2nd Term\Micro Processors\Keil 4\ARM\ARMCC\bin\..\include\stdlib.h
 .\main.o: E:\Courses_Talta 7asebaat\2nd Term\Micro Processors\Keil 4\ARM\ARMCC\bin\..\include\math.h
+.\main.o: E:\Courses_Talta 7asebaat\2nd Term\Micro Processors\Keil 4\ARM\ARMCC\bin\..\include\ctype.h
